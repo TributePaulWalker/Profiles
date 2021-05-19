ef6f083d4dcb1dffb1c4e60a743533594e609b4b
Brian <hexiaoyu27149@sina.cn>
2021-05-19 19:57:28 +0800

    Update Baidu Cloud.js

--- JavaScript/Baidu Cloud.js
+++ JavaScript/Baidu Cloud.js
@@ -4,11 +4,11 @@
 
 Surge 
 [Script]
-百度云VIP倍速播放 = type=http-response,pattern=https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user,requires-body=1,max-size=0,timeout=20,script-path=https://raw.githubusercontent.com/TributePaulWalker/JavaScript/main/Baidu%20Cloud.js
+百度云VIP倍速播放 = type=http-response,pattern=https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user,requires-body=1,max-size=0,timeout=20,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Baidu%20Cloud.js
 
 QuantumultX
 [rewrite_local]
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/TributePaulWalker/JavaScript/main/Baidu%20Cloud.js
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Baidu%20Cloud.js
 
 [MITM]
 hostname = pan.baidu.com
