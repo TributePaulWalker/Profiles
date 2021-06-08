6ede6944602c7db9810c6ed79712b716b88b5cbe
Brian <hexiaoyu27149@sina.cn>
2021-06-09 06:46:32 +0800

    Update Sub_info_lite.js

--- JavaScript/Sub_info_lite.js
+++ JavaScript/Sub_info_lite.js
@@ -6,10 +6,10 @@ Surge配置参考注释，感谢@asukanana,感谢@congcong.
 ----------------------------------------
 
 [Proxy Group]
-AmyInfo = select, policy-path=http://sub.info?url=机场节点链接&reset_day=1&alert=1, update-interval=3600
+Dler = select, policy-path=http://sub.info?url=你encode后的订阅链接&due_day=1&alert=1&title=DlerCloud, update-interval=3600
 
 [Script]
-Sub_info = type=http-request,pattern=http://sub\.info,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info.js
+机场信息 = type=http-request,pattern=http://sub\.info,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Sub_info_lite.js
 ----------------------------------------
 
 脚本不用修改，直接配置就好。
@@ -21,6 +21,8 @@ Sub_info = type=http-request,pattern=http://sub\.info,script-path=https://raw.gi
 可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。
 
 可选参数 &alert，流量用量超过80%、流量重置2天前、流量重置、套餐快到期，这四种情况会发送通知，参数"title=xxx" 可以自定义通知的标题。如"&alert=1&title=AmyInfo",多个机场信息，且需要通知的情况，一定要加 title 参数，不然通知判断会出现问题
+
+订阅encode链接：https://www.urlencoder.org/
 ----------------------------------------
 */
 
@@ -38,7 +40,7 @@ let resetLeft = getRmainingDays(resetDay);
   let total = usage.total;
   let expire = usage.expire || params.expire;
   let localProxy = "=http, localhost, 6152";
-  let infoList = [`使用${bytesToSize(used)} | ${bytesToSize(total)}`];
+  let infoList = [`使用：${bytesToSize(used)} | ${bytesToSize(total)}`];
 
   if (resetLeft) {
     infoList.push(`重置：剩余${resetLeft}天`);
