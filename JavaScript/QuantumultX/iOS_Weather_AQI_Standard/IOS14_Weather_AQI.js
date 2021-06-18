025c4e77a4dd5bc167077bc11bbf1f18fd7c4d9a
Brian <hexiaoyu27149@sina.cn>
2021-06-19 07:55:04 +0800

    Update IOS14_Weather_AQI.js

--- JavaScript/QuantumultX/iOS_Weather_AQI_Standard/IOS14_Weather_AQI.js
+++ JavaScript/QuantumultX/iOS_Weather_AQI_Standard/IOS14_Weather_AQI.js
@@ -9,7 +9,7 @@ const aqicnToken = $.getdata($.token) || '';
 
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
-	[Script]
+	[rewrite_local]
 	iOS14美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v1/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS14_Weather_AQI_US.js
 
 	[MITM]
