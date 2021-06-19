a3ad738bd8d9a70bcb5a69a1adf1a04a68f215ee
Brian <hexiaoyu27149@sina.cn>
2021-06-19 11:57:32 +0800

    Update IOS15_Weather_AQI.js

--- JavaScript/Surge/iOS_Weather_AQI_Standard/IOS15_Weather_AQI.js
+++ JavaScript/Surge/iOS_Weather_AQI_Standard/IOS15_Weather_AQI.js
@@ -7,7 +7,7 @@ const aqicnToken = ''
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	iOS15美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v2/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS15_Weather_AQI_US.js
+	Surge_IOS15_Weather_AQI_Standard = type=http-response,pattern=https://weather-data.apple.com/v2/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS15_Weather_AQI_US.js
 
 	[MITM]
 	hostname = weather-data.apple.com
