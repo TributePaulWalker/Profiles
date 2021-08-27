4255d14253968edbb495ed3a9d699396146c4133
Brian <hexiaoyu27149@sina.cn>
2021-08-27 12:47:18 +0800

    Update Bstar.js

--- JavaScript/Bstar.js
+++ JavaScript/Bstar.js
@@ -1,7 +1,7 @@
 let body = $request.body;
 
 body = body.replace(/&sim_code=\d+/,"&sim_code=51503");
-body = body.replace(/&locale=zh_CN/,"&&locale=zh_PH");
+body = body.replace(/&locale=zh_CN/,"&locale=zh_PH");
 body = body.replace(/&s_locale=zh-Hans_[A-Z]{2}/,"&s_locale=zh-Hans_PH");
 
 console.log(body)
