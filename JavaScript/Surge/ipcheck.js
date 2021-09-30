67eabf06ad74de2ac8a74bd6e453db077eae0fbc
Brian <hexiaoyu27149@sina.cn>
2021-09-30 16:59:46 +0800

    Create ipcheck.js

---
+++ JavaScript/Surge/ipcheck.js
@@ -0,0 +1,26 @@
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.query
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+  body = {
+    title: "节点信息",
+    content: `IP: ${ip}\nISP: ${isp}\n位置: ${emoji}${country} - ${city}`,
+    icon: "network",
+    'icon-color': "#FFD700"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
