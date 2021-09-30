6ea3c0a92aa29fee9e501fae72676b4ed675f9a2
Brian <hexiaoyu27149@sina.cn>
2021-09-30 18:36:46 +0800

    Update and rename JavaScript/Surge/iOS_Weather_AQI_Standard/IOS14_Weather_AQI.js to JavaScript/Surge/IOS14_Weather_AQI.js

--- JavaScript/Surge/IOS14_Weather_AQI.js
+++ JavaScript/Surge/IOS14_Weather_AQI.js
@@ -7,7 +7,7 @@ const aqicnToken = ''
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	Surge_IOS14_Weather_AQI_Standard = type=http-response,pattern=https://weather-data.apple.com/v1/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS14_Weather_AQI_US.js
+	Surge_IOS14_Weather_AQI_Standard = type=http-response,pattern=https://weather-data.apple.com/v1/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/IOS14_Weather_AQI.js
 
 	[MITM]
 	hostname = weather-data.apple.com
