75147f839f7c511f561de5d976115004272819a4
Brian <hexiaoyu27149@sina.cn>
2021-10-04 20:04:19 +0800

    Update Profile-Reload.js

--- JavaScript/Surge/Profile-Reload.js
+++ JavaScript/Surge/Profile-Reload.js
@@ -1,3 +1,13 @@
+/*
+By：Pysta
+
+* [Panel]
+* 配置重载=title="配置重载",content="配置重载",icon:"arrow.triangle.2.circlepath.doc.on.clipboard","icon-color":"#FFD700",style=info,script-name=配置重载,update-interval=-1
+
+* [Script]
+* 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
+*/
+
 $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $notification.post("配置重载","成功","")
     $done({
