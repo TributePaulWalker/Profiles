e79fbfa6d3dc99670ad693b8a9f3c6b6dadda507
Brian <hexiaoyu27149@sina.cn>
2021-10-05 13:02:50 +0800

    Update Profile-Reload.js

--- JavaScript/Surge/Profile-Reload.js
+++ JavaScript/Surge/Profile-Reload.js
@@ -4,7 +4,7 @@ By：Pysta
 GitHub:https://github.com/mieqq/mieqq
 
 * [Panel]
-* 配置重载=title="配置重载",content="配置重载",icon:"arrow.triangle.2.circlepath.doc.on.clipboard","icon-color":"#FFD700",style=info,script-name=配置重载,update-interval=-1
+* 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
 
 * [Script]
 * 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
