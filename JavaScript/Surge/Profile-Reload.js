75230bdfc599a9f1f23902da7e931f2deb784f25
Brian <hexiaoyu27149@sina.cn>
2021-10-05 11:50:57 +0800

    Update Profile-Reload.js

--- JavaScript/Surge/Profile-Reload.js
+++ JavaScript/Surge/Profile-Reload.js
@@ -15,7 +15,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $done({
         title: "配置重载",
         content: "配置重载成功",
-        icon: "gearshape.2",
+        icon: "arrow.rectanglepath",
         "icon-color": "#FFD700",
      })
     });
