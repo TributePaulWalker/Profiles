a2ef5322f45d177ee04e03dc7f546fde0ea9e2f8
Brian <hexiaoyu27149@sina.cn>
2021-11-28 20:15:09 +0800

    Update trafficstatistics.js

--- JavaScript/Surge/trafficstatistics.js
+++ JavaScript/Surge/trafficstatistics.js
@@ -55,7 +55,7 @@ if(net=="en0") {
 
 
   $done({
-      title:"流量统计 | "+netType,
+      title:"流量统计 : "+netType,
       content:`流量 : ${upload} | ${download}\n`+
       `速度 : ${outCurrentSpeed} | ${inCurrentSpeed}\n` +
 		`峰值 : ${outMaxSpeed} | ${inMaxSpeed}`,
