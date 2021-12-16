539c171bb317ce8b8c3d6e45b7c2a265aba4acd3
Brian <hexiaoyu27149@sina.cn>
2021-12-16 12:53:54 +0800

    Update Covid19.js

--- JavaScript/Surge/Covid19.js
+++ JavaScript/Surge/Covid19.js
@@ -15,7 +15,7 @@ function num(location, result) {
   let loc_now_res = loc_now.exec(resu);
   if (loc_new_res) {
     //console.log("已获取" + loc + "的信息");
-    ala = ala +loc +"   :   " +loc_new_res[1].padStart(5,"\u0020")+"/" +loc_now_res[1].padStart(5,"\u0020")+ "\n";
+    ala = ala +loc +"   :   " +loc_new_res[1].padStart(5,"\u0020")+"|" +loc_now_res[1].padStart(5,"\u0020")+ "\n";
   } else {
     //console.log("获取" + loc + "的信息失败");
     ala = ala + loc + "   :   查无数据\n";
