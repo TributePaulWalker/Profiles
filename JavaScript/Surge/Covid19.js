42169893bf4ebfc2b924edc6aeafc73b28dc9942
Brian <hexiaoyu27149@sina.cn>
2021-12-27 15:28:01 +0800

    Update Covid19.js

--- JavaScript/Surge/Covid19.js
+++ JavaScript/Surge/Covid19.js
@@ -29,7 +29,7 @@ $httpClient.get(url, function(error, response, data){
      $done({
        title: "疫情查询:新增|现存"+ "   "+nowtime(),
        icon:"filemenu.and.cursorarrow",
-       "icon-color":"#0089A7",
+       "icon-color":"#5AC8FA",
        content: ala.replace(/\n$/, "")
      });
     }
