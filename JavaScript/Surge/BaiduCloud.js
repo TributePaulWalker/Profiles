9c35b91611dfcb5a6660cbc08eff7624a4f3e7b5
Brian <hexiaoyu27149@sina.cn>
2023-01-16 13:05:22 +0800

    Update BaiduCloud.js

--- JavaScript/Surge/BaiduCloud.js
+++ JavaScript/Surge/BaiduCloud.js
@@ -12,47 +12,49 @@ hostname = pan.baidu.com
 
 QuantumultX
 [rewrite_local]
-# 百度云VIP倍速播放
 https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/BaiduCloud.js
 
 [MITM]
 hostname = pan.baidu.com
+*/
 
-**************************/
+let obj = {
+  "product_infos": [{
+    "product_id": "5310897792128633390",
+    "start_time": 1417260485,
+    "end_time": 1872502399,
+    "buy_time": "1417260485",
+    "cluster": "offlinedl",
+    "detail_cluster": "offlinedl",
+    "product_name": "gz_telecom_exp"
+  }, {
+    "product_name": "svip2_nd",
+    "product_description": "超级会员",
+    "function_num": 0,
+    "start_time": 1553702399,
+    "buy_description": "",
+    "buy_time": 0,
+    "product_id": "1",
+    "auto_upgrade_to_svip": 0,
+    "end_time": 1672502399,
+    "cluster": "vip",
+    "detail_cluster": "svip",
+    "status": 0
+  }],
+  "currenttime": 1573473597,
+  "reminder": {
+    "reminderWithContent": [],
+    "advertiseContent": []
+  },
+  "request_id": 7501873289383874371,
+  "guide_data": {
+    "title": "超级会员 SVIP",
+    "content": "已拥有极速下载+视频倍速特权",
+    "button": {
+      "text": "会员中心",
+      "action_url": "https://pan.baidu.com/wap/vip/user?from=myvip2#svip"
+    }
+  }
+};
 
-if ($response.body) {
-    $done({
-        body: JSON.stringify({
-            "product_infos": [{
-                "product_id": "5310897792128633390",
-                "start_time": 2147483648,
-                "end_time": 2877706811,
-                "buy_time": "1417260485",
-                "cluster": "offlinedl",
-                "detail_cluster": "offlinedl",
-                "product_name": "gz_telecom_exp"
-            }, {
-                "product_name": "svip2_nd",
-                "product_description": "超级会员",
-                "function_num": 0,
-                "start_time": 1672502399,
-                "buy_description": "",
-                "buy_time": 0,
-                "product_id": "1",
-                "auto_upgrade_to_svip": 0,
-                "end_time": 1791302400,
-                "cluster": "vip",
-                "detail_cluster": "svip",
-                "status": 0
-            }],
-            "currenttime": 1672502399,
-            "reminder": {
-                "reminderWithContent": [],
-                "advertiseContent": []
-            },
-            "request_id": 7501873289383874371
-        })
-    });
-} else {
-    $done({});
-}
+$done({body: JSON.stringify(obj)})
