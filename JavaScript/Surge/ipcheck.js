990b1eb77a08b912546d3e16418122465d9706e5
Brian <hexiaoyu27149@sina.cn>
2023-01-23 15:42:11 +0800

    Update ipcheck.js

--- JavaScript/Surge/ipcheck.js
+++ JavaScript/Surge/ipcheck.js
@@ -21,7 +21,7 @@ $httpClient.get(url, function(error, response, data){
   body = {
     title: "网络信息",
     content: `IP信息: ${ip}\n运营商: ${isp}\nIP位置: ${emoji}${country} - ${city}`,
-    icon: "link.icloud",
+    icon: "key.icloud",
     'icon-color': "#5AC8FA"
   }
   $done(body);
