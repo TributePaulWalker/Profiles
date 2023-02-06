0f39a01b7f36741db0c1fef3f07ca04429d93c61
Brian <hexiaoyu27149@sina.cn>
2023-02-06 21:13:09 +0800

    完善使用注释

--- JavaScript/Surge/wnCalendar.js
+++ JavaScript/Surge/wnCalendar.js
@@ -3,6 +3,8 @@
  * @author: zqzess
  * 仓库地址：https://github.com/zqzess/rule_for_quantumultX
  * 感谢@chavyleung提供的Env
+
+ * Surge：
  * 定时任务添加：
  * [Script]
  * 今日黄历播报 = type=cron,timeout=20,cronexp=0 7 * * *,wake-system=1,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/wnCalendar.js
@@ -11,6 +13,14 @@
  * 今日黄历 = script-name=今日黄历,title=今日黄历,content=请刷新,style=info,update-interval=86400
  * [Script]
  * 今日黄历 = type=generic,timeout=20,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/wnCalendar.js
+
+
+ * QuanmutultX：
+ * [task_local]
+ * # 今日黄历
+ * 0 7 * * * https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/wnCalendar.js, tag=今日黄历
+
+
  */
 const $ = new Env('wnCalendar', true)
 let title = '今日黄历'
