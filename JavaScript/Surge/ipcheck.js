76fa580b58dfb8f534e09beb52128398a49e273d
Brian <hexiaoyu27149@sina.cn>
2023-02-06 21:33:55 +0800

    Update ipcheck.js

--- JavaScript/Surge/ipcheck.js
+++ JavaScript/Surge/ipcheck.js
@@ -1,11 +1,14 @@
 /*
 https://raw.githubusercontent.com/congcong0806/surge-list/master/Script/ipcheck.js
 
-* [Panel]
-* 网络信息 = script-name=网络信息, title="网络信息", content="请刷新", style=info, update-interval=60
-* ...
-* [Script]
-* 网络信息 = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/ipcheck.js
+Surge：
+
+[Panel]
+网络信息 = script-name=网络信息, title="网络信息", content="请刷新", style=info, update-interval=60
+
+[Script]
+网络信息 = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/ipcheck.js
+
 */
 
 let url = "http://ip-api.com/json/?lang=zh-CN"
