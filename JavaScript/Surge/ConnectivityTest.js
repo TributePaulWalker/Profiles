24bc310d588769315c1c0a7e40ee364a96608d6d
Brian <hexiaoyu27149@sina.cn>
2023-03-03 08:05:57 +0800

    Update ConnectivityTest.js

--- JavaScript/Surge/ConnectivityTest.js
+++ JavaScript/Surge/ConnectivityTest.js
@@ -4,7 +4,7 @@
 
 Surge:
 [Panel]
-网络延迟 = script-name=网络延迟,update-interval=5
+网络延迟 = script-name=网络延迟,update-interval=1
 
 [Script]
 网络延迟 = type=generic,timeout=5,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/ConnectivityTest.js,argument=title=网络延迟&server=false
