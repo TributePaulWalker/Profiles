be3eb1d07dc01066e7f058d568f18f96ea7c85e5
Brian <hexiaoyu27149@sina.cn>
2023-05-12 19:49:59 +0800

    Update SpeedLimit.js

--- JavaScript/Surge/SpeedLimit.js
+++ JavaScript/Surge/SpeedLimit.js
@@ -1,10 +1,10 @@
 /*
 
 作者：小白脸
-版本：1.0.4
+版本：1.0.3
 搬运：@MuTu888
 仓库：https://github.com/githubdulong
-日期：2023.05.12 07:58
+日期：2023.05.11 14:34
 Surge配置参考注释
 
 示例↓↓↓ 
@@ -42,9 +42,7 @@ const speed = (includes = "?.inCurrentSpeed") => {
       $httpAPI("GET", "/v1/requests/active", null, (data) =>
          r(
             eval(
-               `const Data =
-data.requests.filter(item => item.URL.includes('${host}'));					
-	Data[0]	? Data.reduce((prev, current) => (prev.speed > current.speed) ? prev : current)${includes} : undefined;`,
+               `data.requests.filter(item => item.URL.includes('${host}')).reduce((prev, current) => (prev.speed > current.speed) ? prev : current)${includes}`,
             ),
          ),
       );
@@ -82,80 +80,75 @@ if (_Group && Date.now() - lastUpdateTime >= 0.16 * 3600000) $surge.setSelectGro
 $done({ matched: true });
 
 !(async () => {
-   try {
-      const Group = _Group || (await speed("?.notes")).find((x) => x.includes("->")).match(/path\:\s(.+?)\s->/)[1];
+   const Group = _Group || (await speed(".notes")).find((x) => x.includes("->")).match(/path\:\s(.+?)\s->/)[1];
 
-      if (typeof $argument === "string") {
-         var arg = $argument.match(`${Group}.+?minSpeed=[0-9]+`)?.[0].replace(/\s+/g, "");
+   if (typeof $argument === "string") {
+      var arg = $argument.match(`${Group}.+?minSpeed=[0-9]+`)?.[0].replace(/\s+/g, "");
 
-         if (!arg) throw "策略组不存在";
-      } else {
-         throw "argument不存在,别直接运行";
+      if (!arg) throw "策略组不存在";
+   } else {
+      throw "argument不存在,别直接运行";
+   }
+
+   const { policy, time, minSpeed } = Object.fromEntries(arg.split("&").map((item) => item.split("=")));
+
+   [Group, policy, time, minSpeed].forEach((value, index) => {
+      const _value = ["Group", "Policy", "Time", "MinSpeed"][index];
+      if (!value) {
+         throw `${_value} 不能为空`;
+      } else if (index >= 2 && isNaN(value)) {
+         throw `${_value} 必须为数字`;
       }
+   });
 
-      const { policy, time, minSpeed } = Object.fromEntries(arg.split("&").map((item) => item.split("=")));
+   const arr_policy = policy.split(",").filter((x) => !!x);
 
-      [Group, policy, time, minSpeed].forEach((value, index) => {
-         const _value = ["Group", "Policy", "Time", "MinSpeed"][index];
-         if (!value) {
-            throw `${_value} 不能为空`;
-         } else if (index >= 2 && isNaN(value)) {
-            throw `${_value} 必须为数字`;
-         }
-      });
+   const policy1 = policyGroupName(Group); // 现在使用的
+   const index_p = arr_policy.length;
+   if (index_p < 2) throw "policy必须包含默认策略和至少一个跳转策略";
+   const policy0 = arr_policy[0];
+   const End = arr_policy[index_p - 1];
+   let policys = cache[host]?.policy;
 
-      const arr_policy = policy.split(",").filter((x) => !!x);
+   //存储的
+   if (policy1 === policy0) {
+      policys = policy0;
+   }
 
-      const policy1 = policyGroupName(Group); // 现在使用的
-      const index_p = arr_policy.length;
-      if (index_p < 2) throw "policy必须包含默认策略和至少一个跳转策略";
-      const policy0 = arr_policy[0];
-      const End = arr_policy[index_p - 1];
-      let policys = cache[host]?.policy;
+   //限制并发请求
+   if (cache[host].switch === "1") return;
+   write("1");
 
-      //存储的
-      if (policy1 === policy0) {
-         policys = policy0;
-				 write("0");
+   let current_speed;
+   let count = 0;
+
+   for (let i = 0; i < Math.ceil(time / 3); i++) {
+      await new Promise((r) => setTimeout(r, 3000));
+      current_speed = await speed();
+
+      if (current_speed === undefined || current_speed < 1500) count++;
+
+      if (count >= 2 || policyGroupName(Group) === End || current_speed >= minSpeed * 1048576) {
+         write("0");
+         return;
       }
+   } //主逻辑一直循环策略
+   //网络波动，速度达标，最后个策略 结束循环
 
-      //限制并发请求
-      if (cache[host].switch === "1") return;
-      write("1");
+   const p = arr_policy[arr_policy.indexOf(policy1) + 1];
 
-      let current_speed;
-      let count = 0;
-
-      for (let i = 0; i < Math.ceil(time / 3); i++) {
-         await new Promise((r) => setTimeout(r, 3000));
-         current_speed = await speed();
-
-         if (!current_speed || current_speed < 1500) count++;
-
-         if (count >= 2 || policyGroupName(Group) === End || current_speed >= minSpeed * 1048576) {
-            write("0");
-            return;
-         }
-      } //主逻辑一直循环策略
-      //网络波动，速度达标，最后个策略 结束循环
-
-      const p = arr_policy[arr_policy.indexOf(policy1) + 1];
-
-      if (!p) return;
-      $surge.setSelectGroupPolicy(`${Group}`, `${p}`);
-      $notification.post(
+   if (!p) return;
+   $surge.setSelectGroupPolicy(`${Group}`, `${p}`);
+   $notification.post(
       `策略切换成功 🎉`,
       `速度 ${speed_unit(current_speed)} ${minSpeed} MB/s`,
       `域名 ${host}\n监控时长${time}秒 切换${p}策略`,
-      );
-      cache[host].time = Date.now();
-      cache[host].Group = Group;
-      cache[host].policy = p;
-      cache[host].policy0 = policy0;
-      write("0");
-   } catch (err) {
-      throw "";
-   }
+   );
+   cache[host].time = Date.now();
+   cache[host].Group = Group;
+   cache[host].policy = p;
+   cache[host].policy0 = policy0;
+   write("0");
 })().catch((err) => {
    write("0");
    $notification.post("错误: ❌", err.message || err, "策略切换失败 😞");
